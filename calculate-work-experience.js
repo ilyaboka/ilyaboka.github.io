74d79183e3251725843b4579349f4c8654f6f2e6
Ilya Boka <bokailya@gmail.com>
2023-03-11 07:59:52 +0400

    Initial commit

---
+++ calculate-work-experience.js
@@ -0,0 +1,29 @@
+const firstWorkingDay = new Date(2020, 0, 14);
+const firstWorkingDayOnLastJob = new Date(2021, 7, 30);
+
+function calculateWorkExperience(startDate) {
+    const now = new Date();
+    const totalMonths
+        =
+        (now.getFullYear() - startDate.getFullYear()) * 12
+        + now.getMonth() - startDate.getMonth()
+        - (1 ? startDate.getDate() > now.getDate() : 0)
+    ;
+
+    return {months: totalMonths % 12, years: Math.floor(totalMonths / 12)};
+}
+
+const totalWorkExperience = calculateWorkExperience(firstWorkingDay);
+const workExperienceAtLastJob
+    = calculateWorkExperience(firstWorkingDayOnLastJob);
+
+
+document.getElementById('months-of-total-experience').innerText
+    = totalWorkExperience.months.toString();
+document.getElementById('years-of-total-experience').innerText
+    = totalWorkExperience.years.toString();
+
+document.getElementById('months-of-experience-at-last-job').innerText
+    = workExperienceAtLastJob.months.toString();
+document.getElementById('years-of-experience-at-last-job').innerText
+    = workExperienceAtLastJob.years.toString();
